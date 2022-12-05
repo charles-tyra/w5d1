8e99ade44f9a199db8a32bbc7f6e492549036053
Charles Tyra <charlestyra@MacBook-Pro.local>
2022-12-05 16:06:10 -0500

    working through linked_list class - stuck on #each method

--- hash_lru_cache/lib/p04_linked_list.rb
+++ hash_lru_cache/lib/p04_linked_list.rb
@@ -16,11 +16,18 @@ class Node
   def remove
     # optional but useful, connects previous link to next link
     # and removes self from list.
+    @next.prev = @prev
+    @prev.next = @next
   end
 end
 
 class LinkedList
+include Enumerable
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -29,30 +36,65 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    iternode = @head.next
+    count = 0
+    while iternode != @tail
+      return count if key == iternode.key
+      iternode = iternode.next
+      count += 1
+    end
+    nil
   end
 
   def include?(key)
+    iternode = @head.next
+    while iternode != @tail
+      return true if key == iternode
+      iternode = iternode.next
+    end
+    false
   end
 
   def append(key, val)
+    noodle = Node.new(key, val)
+    noodle.prev = @tail.prev
+    @tail.prev.next = noodle
+    @tail.prev = noodle
   end
 
   def update(key, val)
   end
 
   def remove(key)
+    iternode = @head.next
+    count = 0
+    while iternode != @tail
+      if iternode == key
+        key.remove
+        return count
+      end
+      iternode = iternode.next
+      count += 1
+    end
   end
 
   def each
+    iternode = @head.next
+    while iternode != @tail
+      iternode = iternode.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
