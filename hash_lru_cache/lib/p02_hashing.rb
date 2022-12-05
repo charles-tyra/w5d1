b2503f3c94f576ebcf9d7cc52a1a662f7b97ab75
Charles Tyra <charlestyra@MacBook-Pro.local>
2022-12-05 14:19:40 -0500

    Phase 2 - hashing, created homemade hash methods for the Array, String and Hash class

--- hash_lru_cache/lib/p02_hashing.rb
+++ hash_lru_cache/lib/p02_hashing.rb
@@ -4,11 +4,23 @@ end
 
 class Array
   def hash
+    hash_int = 0
+    (0...length).each do |i|
+      hash_int += self[i].hash * i+1
+    end
+    hash_int
   end
 end
 
 class String
   def hash
+    alpha_index = ('a'..'z').to_a + ('A'..'Z').to_a
+
+    hash_int = 0
+    (0...length).each do |i|
+      hash_int += alpha_index.index(self[i]) / (i+1)
+    end
+    hash_int
   end
 end
 
@@ -16,6 +28,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash_int = 0
+    self.each do |k, v|
+      hash_int += (v.hash * k.hash)
+    end
+    hash_int
   end
 end
