f5fc9f2d5b111b04a4eb46fd8049a32ad21c528f
Charles Tyra <charlestyra@MacBook-Pro.local>
2022-12-05 11:45:22 -0500

    lru-cache inplementation of array base

--- lru_cache.rb
+++ lru_cache.rb
@@ -1,17 +1,26 @@
 class LRUCache
-   def initialize
+   attr_reader :max_size
+   def initialize(n = 4)
+      @cache = []
+      @max_size = n
    end
 
    def count
-      # returns number of elements currently in cache
+      @cache.length
    end
 
    def add(el)
-      # adds element to cache according to LRU principle
+      if !@cache.include?(el)
+         count < max_size ? el : @cache.shift
+         @cache << el
+      else
+         @cache.delete(el)
+         @cache << el
+      end
    end
 
    def show
-      # shows the items in the cache, with the LRU item first
+      @cache
    end
 
    private
@@ -24,7 +33,7 @@ johnny_cache = LRUCache.new(4)
   johnny_cache.add("I walk the line")
   johnny_cache.add(5)
 
-  johnny_cache.count # => returns 2
+  p johnny_cache.count # => returns 2
 
   johnny_cache.add([1,2,3])
   johnny_cache.add(5)
@@ -37,4 +46,4 @@ johnny_cache = LRUCache.new(4)
   johnny_cache.add({a: 1, b: 2, c: 3})
 
 
-  johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the
+  p johnny_cache.show # => prints [[1, 2, 3, 4], :ring_of_fire, "I walk the line", {:a=>1, :b=>2, :c=>3}]
