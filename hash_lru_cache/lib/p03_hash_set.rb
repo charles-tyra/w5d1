bd16a03ac85d7b920f2ad25de3aa6cf070f6e255
Charles Tyra <charlestyra@MacBook-Pro.local>
2022-12-05 14:36:38 -0500

    Hash set class created, waiting for code review

--- hash_lru_cache/lib/p03_hash_set.rb
+++ hash_lru_cache/lib/p03_hash_set.rb
@@ -7,18 +7,28 @@ class HashSet
   end
 
   def insert(key)
+    if !include?(key)
+      self[key] << key
+      @count += 1
+      resize! if count >= num_buckets
+    end
   end
 
   def include?(key)
+    self[key].any?(key)
   end
 
   def remove(key)
+    if self[key].include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num.hash % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +36,12 @@ class HashSet
   end
 
   def resize!
+    num_buckets.times { @store << Array.new }
+    @store.each_with_index do |arr, i|
+      arr.each do |ele|
+        @store[i].delete(ele)
+        self[ele] << ele
+      end
+    end
   end
 end
